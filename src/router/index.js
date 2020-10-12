9e5a7541e36278ea2ebfe7e6b09c52d9d6149f91
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2020-10-12 16:40:22 +0800

    basic ui skeleton

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import About from '@/components/general/About.vue'
+import AllDatasets from '@/components/dataset/AllDatasets.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'About',
+    component: About
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/alldatasets',
+    name: 'AllDatasets',
+    component: AllDatasets
   }
 ]
 
