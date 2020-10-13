e59f33d0b488ce346ed9a6d91a884b20b13db80b
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2020-10-13 18:14:33 +0800

    added checks for adding dataset/datafile

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
 Vue.config.productionTip = false
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
 Vue.prototype.$http = axios
 
 new Vue({
