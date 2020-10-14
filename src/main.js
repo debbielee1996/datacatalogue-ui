30ad1aa694a4204fef34a7302e7a67e8d64b00f1
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2020-10-14 12:24:42 +0800

    updated the add data file function to allow users to preselect data types before uploading

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
+import VuePapaParse from 'vue-papa-parse';
 
 Vue.config.productionTip = false
 Vue.component('ValidationObserver', ValidationObserver);
 Vue.component('ValidationProvider', ValidationProvider);
+Vue.use(VuePapaParse);
 Vue.prototype.$http = axios
 
 new Vue({
