7d461a0f5b925ee504385c32d0960750cff2e5e7
debbielee1996 <62943444+debbielee1996@users.noreply.github.com>
2021-01-05 14:09:01 +0800

    add home to v-container

--- src/components/general/Home.vue
+++ src/components/general/Home.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="home">
-    <h1>Welcome to Data Catalogue!</h1>
-    <v-btn
-      elevation="2"
-      outlined
-      x-large
-      :to="'/adddataset'"
-    >Add new dataset</v-btn>
+    <v-container>
+      <h1>Welcome to Data Catalogue!</h1>
+      <v-btn
+        elevation="2"
+        outlined
+        x-large
+        :to="'/adddataset'"
+      >Add new dataset</v-btn>
 
-    <v-btn
-      elevation="2"
-      outlined
-      x-large
-      :to="'/adddatafile'"
-    >Upload new file to dataset</v-btn>
-
+      <v-btn
+        elevation="2"
+        outlined
+        x-large
+        :to="'/adddatafile'"
+      >Upload new file to dataset</v-btn>
+    </v-container>
   </div>
 </template>
 
