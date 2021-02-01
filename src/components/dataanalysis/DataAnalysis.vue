ab605b994900e7d9f868b5de59c8e31c24de58dd
JiaEn182445u <44602073+JiaEn182445u@users.noreply.github.com>
2021-02-01 14:00:53 +0800

    Display of dashboard

---
+++ src/components/dataanalysis/DataAnalysis.vue
@@ -0,0 +1,19 @@
+<style scoped>
+.dashboard{
+            width: 100%;
+            min-height: 600px;
+            margin: auto;}
+</style>
+<template>
+<v-container>
+    <v-card >
+      <v-card-title>
+        Data And Analysis
+      </v-card-title>
+        <v-spacer></v-spacer>
+     <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/tableauuploadtimeanalysis/Dashboard3?:embed=yes&:tabs=yes&:toolbar=yes" width="auto" height="auto" class="dashboard"></iframe>
+    </v-card>
+</v-container>
+</template>
+
+
--- src/components/general/Home.vue
+++ src/components/general/Home.vue
@@ -1,3 +1,9 @@
+<style scoped>
+.dashboard{
+            width: 80%;
+            min-height: 600px;
+            margin: auto;}
+</style>
 <template>
   <div class="home">
     <v-container>
@@ -16,6 +22,18 @@
         :to="'/adddatafile'"
       >Upload new file to dataset</v-btn>
     </v-container>
+    tableau desktop and it is published to tableau online
+    <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/tableaudesktoptesting/Tableaudesktopdashboard?:embed=yes&:tabs=yes&:toolbar=yes" class="dashboard"></iframe>
+  
+     <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/tableaudesktoptestingdata/Dashboard2?:embed=yes&:tabs=yes&:toolbar=yes" class="dashboard">
+    </iframe>
+    tableau online
+   
+    <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/testing/Dashboard1?:embed=yes&:tabs=yes&:toolbar=yes" class="dashboard">
+    </iframe>
+    
+    tableau public
+    <iframe src="https://public.tableau.com/views/Singaporeregionfortestinghavesomewrongvalue/SingaporeRegion?:language=en&:display_count=y&:origin=viz_share_link" class="dashboard"></iframe>
   </div>
 </template>
 
