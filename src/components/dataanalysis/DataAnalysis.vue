728715cd4bdf914125a441fb610d00c9acc9b906
JiaEn182445u <44602073+JiaEn182445u@users.noreply.github.com>
2021-02-04 15:42:55 +0800

    New dashboard

--- src/components/dataanalysis/DataAnalysis.vue
+++ src/components/dataanalysis/DataAnalysis.vue
@@ -11,7 +11,8 @@
         Data And Analysis
       </v-card-title>
         <v-spacer></v-spacer>
-     <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/tableauuploadtimeanalysis/Dashboard3?:embed=yes&:tabs=yes&:toolbar=yes" width="auto" height="auto" class="dashboard"></iframe>
+        <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacataloganalysisintern/views/UploadTimingAnalysisss/Dashboard1?:embed=yes&:tabs=yes&:toolbar=yes" width="auto" height="auto" class="dashboard"></iframe>
+     <!-- <iframe src="https://prod-apnortheast-a.online.tableau.com/t/datacatalog/views/tableauuploadtimeanalysis/Dashboard3?:embed=yes&:tabs=yes&:toolbar=yes" width="auto" height="auto" class="dashboard"></iframe> -->
     </v-card>
 </v-container>
 </template>
